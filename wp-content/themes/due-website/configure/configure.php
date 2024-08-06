f786004934de53693a7dd19228664e9d0527384e
Juliano <julianofirme23@gmail.com>
2024-08-06 10:34:16 -0300

    feat: populando o acf

--- wp-content/themes/due-website/configure/configure.php
+++ wp-content/themes/due-website/configure/configure.php
@@ -132,3 +132,26 @@ function add_query_var_extract_acf()
   $wp->add_query_var('page_id_to_extract');
 }
 // END
+
+function acf_load_color_field_choices($field)
+{
+	$field['pertence_a_qual_empreendimento'] = array();
+
+	$choices = get_field('empreendimento_nome', 'option', false);
+
+	$choices = explode("\n", $choices);
+
+	$choices = array_map('trim', $choices);
+
+	if (is_array($choices)) {
+
+		foreach ($choices as $choice) {
+
+			$field['pertence_a_qual_empreendimento'][$choice] = $choice;
+		}
+	}
+
+	return $field;
+}
+
+add_filter('acf/load_field/name=pertence_a_qual_empreendimento', 'acf_load_color_field_choices');

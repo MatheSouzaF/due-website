e4c8e85cf7d50a6df82eedb726737fa1a526c00d
Juliano Firme <julianofirme23@gmail.com>
2024-08-21 14:21:05 -0300

    fix: ajustando a renderização dos cards no frontend

--- wp-content/themes/due-website/assets/src/js/pages/empreendimento/empreendimento.js
+++ wp-content/themes/due-website/assets/src/js/pages/empreendimento/empreendimento.js
@@ -5,59 +5,74 @@ async function empreendimentoPage() {
   console.warn('Módulo Empreendimento Iniciado!');
 
   try {
-    const empreendimentosData = await getEmpreendimentos();
+
+    // Empreendimentos passados via wp_localize
+    const empreendimentosData = EmpreendimentosData.empreendimentos;
 
     function renderEmpreendimentos(empreendimentos) {
-      $('.empreendimentos').html('');
+      const $container = $('.empreendimentos.cards');
+      $container.html('');
+      console.log("Container limpo.");
+
+      if (empreendimentos.length === 0) {
+        console.log("Nenhum empreendimento para exibir.");
+        return;
+      }
 
       empreendimentos.forEach(function (empreendimento) {
-        const cardTemplate = $('#empreendimento-template').contents().clone(true);
+        const template = document.getElementById('empreendimento-template');
+        const cardTemplate = template.content.cloneNode(true);
 
-        cardTemplate.find('.nome-empreendimento').text(empreendimento.name);
-        cardTemplate.find('.localizacao-empreendimento').text(empreendimento.location);
-        cardTemplate.find('.label-informativo').text(empreendimento.status);
-        cardTemplate.find('.info-quartos').text(`${empreendimento.rooms[0].minimo_de_quartos} a ${empreendimento.rooms[0].maximo_de_quartos} quartos`);
-        cardTemplate.find('.info-tamanho').text(`${empreendimento.size[0].metragem_minima} a ${empreendimento.size[0].metragem_maxima}m²`);
-        cardTemplate.find('.valor').text(empreendimento.offer);
+        $(cardTemplate).find('.nome-empreendimento').text(empreendimento.name || 'N/A');
+        $(cardTemplate).find('.localizacao-empreendimento').text(empreendimento.location || 'N/A');
+        $(cardTemplate).find('.label-informativo').text(empreendimento.status || 'N/A');
 
-        // if (empreendimento.photo) {
-        //   cardTemplate.find('.imagem-empreendimento').attr('src', empreendimento.photo.url).show();
-        // } else {
-        //   cardTemplate.find('.imagem-empreendimento').hide();
-        // }
+        const quartos = empreendimento.rooms && empreendimento.rooms[0];
+        $(cardTemplate).find('.info-quartos').text(quartos ? `${quartos.minimo_de_quartos} a ${quartos.maximo_de_quartos} quartos` : 'N/A');
 
-        // if (empreendimento.video) {
-        //   cardTemplate.find('.video-empreendimento').attr('src', empreendimento.video).show();
-        // } else {
-        //   cardTemplate.find('.video-empreendimento').hide();
-        // }
+        const tamanho = empreendimento.size && empreendimento.size[0];
+        $(cardTemplate).find('.info-tamanho').text(tamanho ? `${tamanho.metragem_minima} a ${tamanho.metragem_maxima}m²` : 'N/A');
 
-        $('.empreendimentos').append(cardTemplate);
+        $(cardTemplate).find('.valor').text(empreendimento.offer || 'N/A');
+
+        if (empreendimento.photo && empreendimento.photo.url) {
+          $(cardTemplate).find('.imagem-empreendimento').attr('src', empreendimento.photo.url);
+        } else {
+          $(cardTemplate).find('.imagem-empreendimento').hide();
+        }
+
+        if (empreendimento.video) {
+          $(cardTemplate).find('.video-empreendimento').attr('src', empreendimento.video);
+        } else {
+          $(cardTemplate).find('.video-empreendimento').hide();
+        }
+
+        $container.append(cardTemplate);
       });
     }
 
     function populateFilterOptions() {
       const locationOptions = [...new Set(empreendimentosData.map(e => e.location))];
       const statusOptions = [...new Set(empreendimentosData.map(e => e.status))];
-    
+
       const roomsOptions = new Set();
       empreendimentosData.forEach(e => {
         const minimo = parseInt(e.rooms[0].minimo_de_quartos);
         const maximo = parseInt(e.rooms[0].maximo_de_quartos);
-        
+
         for (let i = minimo; i <= maximo; i++) {
           roomsOptions.add(i);
         }
       });
-    
+
       const options = {
         'filter-location': Array.from(locationOptions).map(location => ({ value: location, label: location })),
         'filter-status': Array.from(statusOptions).map(status => ({ value: status, label: status })),
-        'filter-rooms': Array.from(roomsOptions).map(room => ({ value: room, label: `${room} Quartos` }))
+        'filter-rooms': Array.from(roomsOptions).sort().map(room => ({ value: room, label: `${room} Quartos` }))
       };
 
       return options
-    }    
+    }
 
     populateFilterOptions()
 

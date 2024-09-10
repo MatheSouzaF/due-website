320ce7cbb891ba9e130e6f6ddbe91024f525e6fa
Juliano Firme <julianofirme23@gmail.com>
2024-09-10 10:38:34 -0300

    fix assets version

--- wp-content/themes/due-website/assets_version.php
+++ wp-content/themes/due-website/assets_version.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php define("ASSETS_VERSION", "1725971090068");
-=======
-<?php define("ASSETS_VERSION", "1725970725777");
->>>>>>> 6a75a63f88fe5586606050cb67fbb78660af620b
+<?php define("ASSETS_VERSION", "1725975316922");

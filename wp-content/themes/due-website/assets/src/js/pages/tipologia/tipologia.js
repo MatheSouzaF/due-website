a7fb297b95eeca3fa9ac42bb807eb3ff01e0b4a3
Juliano Firme <julianofirme23@gmail.com>
2024-09-11 12:20:44 -0300

    fix: filtro das tipologias

--- wp-content/themes/due-website/assets/src/js/pages/tipologia/tipologia.js
+++ wp-content/themes/due-website/assets/src/js/pages/tipologia/tipologia.js
@@ -182,27 +182,25 @@ async function tipologiaPage() {
         const matchLocation = !locationFilter || locationFilter.includes(tipologia.location.toLowerCase());
         const matchStatus = !statusFilter || statusFilter.includes(tipologia.status.toLowerCase());
         const matchEmpreendimento = !empreendimentoFilter || empreendimentoFilter.includes(tipologia.project.toLowerCase());
-        const matchRooms =
-        !roomsFilter ||
-        tipologia.isStudio && roomsFilter.includes('studio') || 
-        tipologia.rooms.some(
-          (room) => {
-            const minQuartos = parseInt(room.minimo_de_quartos_tipologia);
-            const maxQuartos = parseInt(room.maximo_de_quartos_tipologia);
+      
+        const matchRooms = !roomsFilter || 
+          (roomsFilter.includes('studio') ? tipologia.isStudio : 
+          tipologia.rooms.some((room) => {
+            const minQuartos = parseInt(room.minimo_de_quartos_tipologia, 10);
+            const maxQuartos = parseInt(room.maximo_de_quartos_tipologia, 10);
             return roomsFilter.some(
               (selectedRoom) => selectedRoom >= minQuartos && selectedRoom <= maxQuartos
             );
-          }
+          })
         );
       
         const matchDiferenciais =
-          !diferenciaisFilter ||
+          !diferenciaisFilter || 
           diferenciaisFilter.every((diff) => tipologia.diffs.includes(diff));
-
+      
         return matchLocation && matchStatus && matchEmpreendimento && matchDiferenciais && matchRooms;
-      });
+      });      
     }
-
 
     function generateBadge(filterValue, filterType) {
       let badgeLabel = filterValue;

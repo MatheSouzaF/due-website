9b8ff4a492e6cd302c77c9ad08f16f066fbd4aeb
Juliano Firme <julianofirme23@gmail.com>
2024-09-13 19:04:45 -0300

    feat: extraindo a função de resetar os filtros

--- wp-content/themes/due-website/assets/src/js/pages/empreendimento/tab-toggle.js
+++ wp-content/themes/due-website/assets/src/js/pages/empreendimento/tab-toggle.js
@@ -1,3 +1,5 @@
+import { resetCheckboxesEmpreendimentos, resetCheckboxesTipologia } from "./reset-filters"
+
 function initTabToggle() {
   console.log('Init tab toggle')
 
@@ -6,80 +8,6 @@ function initTabToggle() {
 
   const $showEmpreendimento = $(".show-empreendimentos")
   const $showTipologia = $(".show-tipologias")
-
-  function resetCheckboxesEmpreendimentos() {
-    const params = new URLSearchParams(window.location.search);
-    const locationFilter = params.get('localizacao') ? params.get('localizacao').split(',').map(v => v.replace(/_/g, ' ').replace(/%/g, '')) : [];
-    const statusFilter = params.get('estagio') ? params.get('estagio').split(',').map(v => v.replace(/_/g, ' ').replace(/%/g, '')) : [];
-    const roomsFilter = params.get('qtos') ? params.get('qtos').split(',').map(v => v.replace(/_/g, ' ').replace(/%/g, '')) : [];
-
-    $(".ckkBox").each(function () {
-      const $checkbox = $(this);
-      const value = $checkbox.val();
-      const isChecked = $checkbox.prop('checked');
-
-      if (
-        $checkbox.closest('#filter-location').length &&
-        locationFilter.indexOf(value) === -1
-      ) {
-        if (isChecked) {
-          $checkbox.click();
-        }
-      }
-
-      if (
-        $checkbox.closest('#filter-status').length &&
-        statusFilter.indexOf(value) === -1
-      ) {
-        if (isChecked) {
-          $checkbox.click();
-        }
-      }
-
-      if (
-        $checkbox.closest('#filter-rooms').length &&
-        roomsFilter.indexOf(value) === -1
-      ) {
-        if (isChecked) {
-          $checkbox.click();
-        }
-      }
-    });
-  }
-
-  function resetCheckboxesTipologia() {
-    const params = new URLSearchParams(window.location.search);
-
-    const locationFilter = params.get('localizacao') ? params.get('localizacao').split(',').map(v => v.replace(/_/g, ' ').replace(/%/g, '')) : [];
-    const statusFilter = params.get('estagio') ? params.get('estagio').split(',').map(v => v.replace(/_/g, ' ').replace(/%/g, '')) : [];
-    const empreendimentoFilter = params.get('empreendimento') ? params.get('empreendimento').split(',').map(v => v.replace(/_/g, ' ').replace(/%/g, '')) : [];
-    const diferenciaisFilter = params.get('diferenciais') ? params.get('diferenciais').split(',').map(v => v.replace(/_/g, ' ').replace(/%/g, '')) : [];
-    const roomsFilter = params.get('qtos') ? params.get('qtos').split(',').map(v => v.replace(/_/g, ' ').replace(/%/g, '')) : [];
-
-    $(".ckkBox").each(function () {
-      const $checkbox = $(this);
-      const value = $checkbox.val();
-      const isChecked = $checkbox.prop('checked');
-
-      const isLocationFilter = $checkbox.closest('#tipologia-filter-location').length && locationFilter.includes(value);
-      const isStatusFilter = $checkbox.closest('#tipologia-filter-status').length && statusFilter.includes(value);
-      const isEmpreendimentoFilter = $checkbox.closest('#tipologia-filter-empreendimento').length && empreendimentoFilter.includes(value);
-      const isDiferenciaisFilter = $checkbox.closest('#tipologia-filter-diferenciais').length && diferenciaisFilter.includes(value);
-      const isRoomsFilter = $checkbox.closest('#tipologia-filter-rooms').length && roomsFilter.includes(value);
-
-      if (
-        (!isLocationFilter && $checkbox.closest('#tipologia-filter-location').length) ||
-        (!isStatusFilter && $checkbox.closest('#tipologia-filter-status').length) ||
-        (!isEmpreendimentoFilter && $checkbox.closest('#tipologia-filter-empreendimento').length) ||
-        (!isDiferenciaisFilter && $checkbox.closest('#tipologia-filter-diferenciais').length) ||
-        (!isRoomsFilter && $checkbox.closest('#tipologia-filter-rooms').length)
-      ) {
-        if (isChecked) {
-          $checkbox.click();
-        }
-      }
-    });
-  }
 
   function showEmpreendimento({ tabRender }) {
     resetCheckboxesEmpreendimentos();
@@ -97,6 +25,11 @@ function initTabToggle() {
 
   function showTipologia({ tabRender }) {
     resetCheckboxesTipologia();
+    
+    let url = new URL(window.location.href);
+    let params = new URLSearchParams(url.search);
+    
+    params.set('tipologia', 'true')
 
     $abaEmpreendimento.hide();
     $abaTipologia.show();
@@ -109,10 +42,12 @@ function initTabToggle() {
     }
   }
 
-  const params = new URLSearchParams(window.location.search);
-  const isTipologia = params.get('tipologia');
+  function getIsTipologia() {
+    const params = new URLSearchParams(window.location.search);
+    return Boolean(params.get('tipologia'));
+  }
 
-  if (isTipologia) {
+  if (getIsTipologia()) {
     showTipologia({ tabRender: false });
   } else {
     showEmpreendimento({ tabRender: false });

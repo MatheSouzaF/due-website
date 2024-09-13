d17d428c37e8051f290a31c954d2dfac2a7c49ff
Juliano Firme <julianofirme23@gmail.com>
2024-09-13 09:19:09 -0300

    feat: implementando a rota dos 100% vendidos

--- wp-content/themes/due-website/app/controllers/EmpreendimentoController.php
+++ wp-content/themes/due-website/app/controllers/EmpreendimentoController.php
@@ -67,6 +67,66 @@ class EmpreendimentoController
     }
 
     /**
+     * Obtém todos os dados de todos os empreendimentos disponíveis.
+     * 
+     * @return array Um array associativo contendo as informações de todos os empreendimentos.
+     */
+    public function getAllAvailableProjects()
+    {
+        $projects = [];
+
+        // Define os argumentos para a consulta de empreendimentos
+        $args = array(
+            'post_type' => 'empreendimentos',
+            'posts_per_page' => -1, // Obtém todos os posts
+            'post_status' => 'publish' // Apenas posts publicados
+        );
+
+        // Executa a consulta
+        $query = new WP_Query($args);
+
+        // Se houver posts, percorre todos eles
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+
+                // Obtém o ID do post
+                $projectId = get_the_ID();
+
+                // Obtém os campos ACF
+                $name = get_field('empreendimento_nome');
+                $location = get_field('localizacao_emprendimento');
+                $isStudio = get_field('e_um_studio');
+                $rooms = get_field('quantidade_de_quartos');
+                $size = get_field('metragem');
+                $status = get_field('estagio_da_obra');
+                $offer = get_field('oferta');
+                $photo = get_field('foto_empreendimento');
+                $video = get_field('video_empreendimento');
+
+                // Adiciona os dados ao array de projetos
+                if ($status !== '100% vendido') {
+                    $projects[] = array(
+                        'ID' => $projectId,
+                        'name' => $name,
+                        'location' => $location,
+                        'isStudio' => $isStudio,
+                        'rooms' => $rooms,
+                        'size' => $size,
+                        'status' => $status,
+                        'offer' => $offer,
+                        'photo' => $photo,
+                        'video' => $video
+                    );
+                }
+            }
+            wp_reset_postdata(); // Reseta os dados globais do post
+        }
+
+        return $projects; // Retorna o array de empreendimentos
+    }
+
+    /**
      * Obtém todos os dados de um empreendimento específico.
      * 
      * @return array Um array associativo contendo as informações de um único empreendimento.

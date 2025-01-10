e4b4293c25f1d597f6b88f16724f83d8e874d75b
matheussf <matheus@dzigual.com.br>
2025-01-10 17:53:33 -0300

    select

--- wp-content/themes/due-website/assets/src/js/pages/select/select.js
+++ wp-content/themes/due-website/assets/src/js/pages/select/select.js
@@ -72,10 +72,63 @@ function swiperEmpreendimentos() {
     },
   });
 }
+function videoFull() {
+  $('.box-button').on('click', function (event) {
+    event.preventDefault(); // Evita que o link siga o href
+
+    // Animação de entrada
+    gsap.to('.box-youtube-vivencie', {
+      duration: 0.8,
+      scale: 1,
+      opacity: 1,
+      ease: 'power3.out',
+      onStart: function () {
+        $('.box-youtube-vivencie').css('pointer-events', 'all'); // Permite interação
+      },
+    });
+
+    // Adiciona a classe 'vivencie-modal' ao elemento '.vivencie'
+    $('.sobre-a-due').addClass('vivencie-modal');
+    $('.box-youtube-vivencie').addClass('box-youtube-vivencie-modal');
+
+    lenis.stop(); // Pausa o scroll suave
+  });
+
+  $('.svg-close').on('click', function (event) {
+    event.preventDefault(); // Evita que o link siga o href
+
+    // Função para parar o vídeo
+    function stopYouTubeVideo() {
+      var $iframe = $('#youtube-player');
+      var src = $iframe.attr('src'); // Captura o src atual
+      $iframe.attr('src', ''); // Remove o src para parar o vídeo
+      $iframe.attr('src', src); // Reatribui o src para resetar o player
+    }
+
+    stopYouTubeVideo();
+    // Animação de saída
+    gsap.to('.box-youtube-vivencie', {
+      duration: 0.8,
+      scale: 0,
+      opacity: 0,
+      ease: 'power3.in',
+      onComplete: function () {
+        $('.box-youtube-vivencie').css('pointer-events', 'none'); // Remove interação
+      },
+    });
+
+    // Remove a classe 'vivencie-modal' do elemento '.vivencie'
+    $('.sobre-a-due').removeClass('vivencie-modal');
+    $('.box-youtube-vivencie').removeClass('box-youtube-vivencie-modal');
+
+    lenis.start(); // Retoma o scroll suave
+  });
+}
 
 function initSelect() {
   select();
   swiperEmpreendimentos();
+  videoFull();
 }
 
 export {initSelect};

70f0eb4a4a0e6b7cc5666ec1ec35b36fc5f3b2ab
Youry Tamisier <youry@dizgual.com.br>
2025-01-16 10:51:23 -0300

    criando pagina select

---
+++ wp-content/themes/due-website/header-select.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html <?php language_attributes(); ?>>
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+	<meta name="format-detection" content="telephone=no">
+	<title><?php wp_title(); ?></title>
+	<?php wp_head(); ?>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.4/lottie.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+
+</head>
+<body <?php body_class(""); ?>>
+    <header class="header-select">
+
+    </header>
+<main class="main">

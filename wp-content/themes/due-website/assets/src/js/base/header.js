f24fcc121c8a044cef68064cca4548864453e47a
matheussf <matheus@dzigual.com.br>
2025-02-26 17:51:29 -0300

    Refatoração da função hoverDestinos e adição de estilos para hover-box-cards

--- wp-content/themes/due-website/assets/src/js/base/header.js
+++ wp-content/themes/due-website/assets/src/js/base/header.js
@@ -33,51 +33,6 @@ function menuSticky() {
     }
   });
 }
-function hoverDestinos() {
-  var tl = gsap.timeline({paused: true});
-  tl.to('header', {
-    duration: 0.2,
-    height: '590px',
-    backgroundColor: '#faf2eb',
-    ease: 'power1.inOut',
-    onStart: function () {
-      $('header').addClass('hover-destinos'); // Adiciona a classe no início da animação
-    },
-    onReverseComplete: function () {
-      $('header').removeClass('hover-destinos'); // Remove a classe quando a animação reverte
-    },
-  });
-  tl.to('.box-video-destinos', {
-    duration: 0.2,
-    zIndex: 4, // Aumenta o z-index para 1
-    ease: 'power1.inOut',
-    pointerEvents: 'initial',
-  });
-  tl.to('.row-videos', {
-    duration: 0.2,
-    clipPath: 'inset(0 0 -100px -100px)',
-    ease: 'power1.inOut',
-  });
-
-  function revertAnimation() {
-    tl.timeScale(3); // Aumenta a velocidade da animação reversa
-    tl.reverse();
-  }
-
-  $('#destinos').hover(function () {
-    tl.play();
-  });
-
-  $('header').mouseleave(function () {
-    revertAnimation();
-  });
-
-  $('.btn-menu-navlink')
-    .not('#destinos')
-    .hover(function () {
-      revertAnimation();
-    });
-}
 
 function animationFooter() {
   function siteFooter() {
@@ -172,6 +127,53 @@ function fabFixed() {
     customElements.define('fab-button', FabButton);
   }
 }
+
+function hoverDestinos() {
+  var tl = gsap.timeline({paused: true});
+  tl.to('header', {
+    duration: 0.2,
+    height: '590px',
+    backgroundColor: '#faf2eb',
+    ease: 'power1.inOut',
+    onStart: function () {
+      $('header').addClass('hover-destinos'); // Adiciona a classe no início da animação
+    },
+    onReverseComplete: function () {
+      $('header').removeClass('hover-destinos'); // Remove a classe quando a animação reverte
+    },
+  });
+  tl.to('.box-video-destinos', {
+    duration: 0.2,
+    zIndex: 4, // Aumenta o z-index para 1
+    ease: 'power1.inOut',
+    pointerEvents: 'initial',
+  });
+  tl.to('.row-videos', {
+    duration: 0.2,
+    clipPath: 'inset(0 0 -100px -100px)',
+    ease: 'power1.inOut',
+  });
+
+  function revertAnimation() {
+    tl.timeScale(3); // Aumenta a velocidade da animação reversa
+    tl.reverse();
+  }
+
+  $('#destinos').hover(function () {
+    tl.play();
+  });
+
+  $('header').mouseleave(function () {
+    revertAnimation();
+  });
+
+  $('.btn-menu-navlink')
+    .not('#destinos')
+    .hover(function () {
+      revertAnimation();
+    });
+}
+
 function hoverJeitoDue() {
   var tl = gsap.timeline({paused: true});
 
@@ -212,49 +214,64 @@ function hoverJeitoDue() {
     },
     '-=1.8'
   );
+  function revertAnimation() {
+    tl.timeScale(4); // Aumenta a velocidade da animação reversa
+    tl.reverse();
+  }
+  $('#jeito-due').hover(function () {
+    tl.play();
+  });
 
+  $('header').mouseleave(function () {
+    revertAnimation();
+  });
+  $('.btn-menu-navlink')
+    .not('#jeito-due')
+    .hover(function () {
+      revertAnimation();
+    });
   // Ao entrar no link com ID "jeito-due"
-  $('#jeito-due').on('mouseenter', function (e) {
-    e.preventDefault(); // Previne o comportamento padrão do link
-    $('.header').addClass('hover-jeito-due'); // Adiciona a classe "hover-jeito-due"
+  // $('#jeito-due').on('mouseenter', function (e) {
+  //   e.preventDefault(); // Previne o comportamento padrão do link
+  //   $('.header').addClass('hover-jeito-due'); // Adiciona a classe "hover-jeito-due"
 
-    // Reinicia a animação do início
-    tl.restart();
-  });
+  //   // Reinicia a animação do início
+  //   tl.restart();
+  // });
 
-  // Quando o mouse sai do <.header>
-  $('.header').on('mouseleave', function () {
-    // Aplica uma animação de saída direta ao .header
-    gsap.to('.header', {
-      height: '72px', // Reverte a altura para o valor original
-      backgroundColor: 'initial', // Reverte a cor de fundo
-      duration: 0.2,
-      ease: 'power1.inOut',
-      onComplete: function () {
-        $('.header').removeClass('hover-jeito-due'); // Remove a classe "hover-jeito-due" após a animação
-        $('.header').removeClass('hover-box-cards'); // Remove a classe "hover-box-cards" após a animação
-      },
-    });
+  // // Quando o mouse sai do <.header>
+  // $('.header').on('mouseleave', function () {
+  //   // Aplica uma animação de saída direta ao .header
+  //   gsap.to('.header', {
+  //     height: '72px', // Reverte a altura para o valor original
+  //     backgroundColor: 'initial', // Reverte a cor de fundo
+  //     duration: 0.2,
+  //     ease: 'power1.inOut',
+  //     onComplete: function () {
+  //       $('.header').removeClass('hover-jeito-due'); // Remove a classe "hover-jeito-due" após a animação
+  //       $('.header').removeClass('hover-box-cards'); // Remove a classe "hover-box-cards" após a animação
+  //     },
+  //   });
 
-    // Remove a opacidade de todos os elementos simultaneamente
-    gsap.to('.header .some-element', {
-      opacity: 0,
-      duration: 0.2,
-      ease: 'power1.inOut',
-      onComplete: function () {
-        tl.pause(0); // Reseta a timeline para o estado inicial
-      },
-    });
+  //   // Remove a opacidade de todos os elementos simultaneamente
+  //   gsap.to('.header .some-element', {
+  //     opacity: 0,
+  //     duration: 0.2,
+  //     ease: 'power1.inOut',
+  //     onComplete: function () {
+  //       tl.pause(0); // Reseta a timeline para o estado inicial
+  //     },
+  //   });
 
-    gsap.to('.header .some-image', {
-      opacity: 0,
-      duration: 0.2,
-      ease: 'power1.inOut',
-      onComplete: function () {
-        tl.pause(0); // Reseta a timeline para o estado inicial
-      },
-    });
-  });
+  //   gsap.to('.header .some-image', {
+  //     opacity: 0,
+  //     duration: 0.2,
+  //     ease: 'power1.inOut',
+  //     onComplete: function () {
+  //       tl.pause(0); // Reseta a timeline para o estado inicial
+  //     },
+  //   });
+  // });
 }
 
 function initHeader() {

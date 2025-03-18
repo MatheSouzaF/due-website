0433e87b16f34798cc06bf254e5e9a7226333b92
Juliano Firme <julianofirme23@gmail.com>
2025-03-18 10:33:56 -0300

    removendo traduções desnecessárias

--- wp-content/themes/due-website/template-empreendimentos/page.php
+++ wp-content/themes/due-website/template-empreendimentos/page.php
@@ -147,14 +147,14 @@ wp_localize_script('main', 'TipologiasData', array(
 
       $bannerId = get_the_ID();
       $banners[] = array(
-        'location' => __(get_field('nome_do_destino'), 'due-website'),
-        'media' => __(get_field('foto_ou_video_do_destino'), 'due-website'),
-        'title' => __(get_field('titulo_do_banner'), 'due-website'),
-        'svg_rota' => __(get_field('svg_rota_due'), 'due-website'),
-        'description' => __(get_field('descricao_do_banner'), 'due-website'),
-        'link' => __(get_field('link'), 'due-website'),
-        'comments' => __(get_field('comentarios_do_banner'), 'due-website'),
-        'svg_caribe_logo' => __(get_field('svg_caribe'), 'due-website'),
+        'location' => get_field('nome_do_destino'), 'due-website',
+        'media' => get_field('foto_ou_video_do_destino'), 'due-website',
+        'title' => get_field('titulo_do_banner'), 'due-website',
+        'svg_rota' => get_field('svg_rota_due'), 'due-website',
+        'description' => get_field('descricao_do_banner'), 'due-website',
+        'link' => get_field('link'), 'due-website',
+        'comments' => get_field('comentarios_do_banner'), 'due-website',
+        'svg_caribe_logo' => get_field('svg_caribe'), 'due-website',
       );
     }
     wp_reset_postdata();
